ad92f2b9c27e2163fafe0ad358456e657ce6e4f8
cryometsys <metsysgaming@gmail.com>
2024-11-28 23:26:57 +0600

    2007032 first commit

--- ContentView.swift
+++ ContentView.swift
@@ -34,60 +34,43 @@ struct ContentView: View {
                 .offset(y: -350)
             
             VStack(spacing: 20) {
-       
-                // shawcha code
+                
+                Text("khulun")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40,weight: .bold,design: .rounded))
+                    .offset(x: -100, y: -100)
+                
+                TextField("Email ", text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty){
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
                 Rectangle()
                     .frame(width: 350, height: 1)
                     .foregroundColor(.white)
-                Button{
-                    register()
-                    
-                }label: {
-                    Text("Sign up")
-                        .bold()
-                        .frame(width: 200, height: 40)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                .fill(.linearGradient(colors: [.pink, .red],startPoint: .top, endPoint: .bottomTrailing))
-                            )
-                        .foregroundColor(.white)
-                }
-                .padding(.top)
-                .offset(y: 110)
-                
-                Button{
-                    
-                }label: {
-                    Text("Already have an account? Login")
-                        .bold()
-                        .foregroundColor(.white)
-                }
-                .padding(.top)
-                .offset(y: 110)
-                
-            }
-            .frame(width: 350)
-            .onAppear{
-                Auth.auth().addStateDidChangeListener { auth, user in
-                    if user != nil {
-                        userIsLoggedIn.toggle()
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
                     }
-                }
-            }
-        }
-        .ignoresSafeArea()
-      
-    }
-
-    // shawcha code
-    func register() {
-        Auth.auth().createUser(withEmail: email, password: password) {
+       
+// mosabbir  code
+    func login(){
+        Auth.auth().signIn(withEmail: email, password: password){
             result, error in
             if error != nil {
                 print(error!.localizedDescription)
             }
         }
-    }}
+    }
+  
 }
 
 struct ContentView_Previews: PreviewProvider{
